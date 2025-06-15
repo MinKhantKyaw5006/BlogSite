f0ea1a9474a28d029ac582a79cb661054d88c1b7
MinKhantKyaw <minkhant050603@gmail.com>
2025-06-16 01:57:56 +0700

    finished kinde auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
 //import Image from "next/image";
-
+import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
 import { Button } from "@/components/ui/button";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const {getUser} = getKindeServerSession()
+  const session = await getUser()
   return (
-    <div>
+    <div className="p-10">
       <h1>Hello</h1>
-      <Button>Hello button</Button>
-      <h2>yes</h2>
+      {session ? (
+        <LogoutLink>
+          <Button>Logout</Button>
+        </LogoutLink>
+      ):(
+        <div>
+          <RegisterLink>
+        <Button>Register</Button>
+      </RegisterLink>
+      <LoginLink>
+        <Button>Login</Button>
+      </LoginLink>
+        </div>
+      )}
     </div>
   );
 }
